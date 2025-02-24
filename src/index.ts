6cdb5bdf4fc1eab036fe1dab29e714e1910cd91a
peppapig450 <nbgenius1@gmail.com>
2025-02-24 14:08:17 -0800

    customize hello world

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import joplin from 'api';
 joplin.plugins.register({
 	onStart: async function() {
 		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		console.info('Hello world. Color Code Plugin started!');
 	},
 });
